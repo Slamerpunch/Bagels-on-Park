3c214656b465572322132ca17c800cbd3c0e6562
Slamerpunch <61360469+Slamerpunch@users.noreply.github.com>
2020-11-09 19:35:42 -0500

    Add files via upload
    checks number if it is divisible

---
+++ NumChecker.java
@@ -0,0 +1,29 @@
+
+
+public class NumChecker
+{
+	public static void main(String[]args){
+		System.out.println(check(200));
+		System.out.println(check(100));
+		System.out.println(check(25));
+		System.out.println(check(88));
+		System.out.println(check(99));
+		System.out.println(check(7));
+		System.out.println(check(8));
+		System.out.println(check(10));
+	
+	}
+	public static int check( int x )
+	{
+		if(x % 5 ==0){
+			return 1;
+		}
+		if(x % 4 ==0){
+			return 2;
+		}
+		if(x % 3 ==0){
+			return 3;
+		}
+		return 4;
+	}
+}
